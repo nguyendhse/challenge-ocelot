1dfb72934df996cdcacd150d695207854488f706
Nguyen Do <nguyen.do@orgit.ai>
2023-11-17 01:01:19 +0700

    init minimal source with django cookie cutter

---
+++ challenge_ocelot/challenge_ocelot/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from challenge_ocelot.users.models import User
+from challenge_ocelot.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
